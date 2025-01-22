af3efb8a47dd2c1bcef2cc2ceac272b5ab0ad3e6
Luis Sacristán <displaynone@gmail.com>
2025-01-22 17:52:38 +0100

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import ImageDiff from './components/ImageDiff';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Image Diff</h1>
+    <div className="App min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800 mb-8">Image Diff</h1>
       <ImageDiff />
     </div>
   );
